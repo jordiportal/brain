adeafd768a83f6866a13f368f30553daefc2c198
jordiportalsole <jordiportalsole@gmail.com>
2026-01-28 16:24:57 +0100

    fix: configure Strapi routes for brain-api-key and brain-model-config

--- services/strapi/src/api/brain-model-config/routes/brain-model-config.ts
+++ services/strapi/src/api/brain-model-config/routes/brain-model-config.ts
@@ -1,7 +1,30 @@
 /**
  * brain-model-config router
+ * 
+ * Rutas con API Token authentication
  */
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::brain-model-config.brain-model-config');
+// Custom routes para single type
+const customRoutes = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/brain-model-config',
+      handler: 'brain-model-config.find',
+    },
+    {
+      method: 'PUT',
+      path: '/brain-model-config',
+      handler: 'brain-model-config.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/brain-model-config',
+      handler: 'brain-model-config.delete',
+    }
+  ]
+};
+
+export default customRoutes;
